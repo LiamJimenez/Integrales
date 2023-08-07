f35cfc68be9310be2bf8572c719a6744efc79f83
LiamJimenez <liammarcos@gmail.com>
2023-08-07 10:30:07 -0400

    Actualizar

--- Integrales-master/liam.py
+++ Integrales-master/liam.py
@@ -7,15 +7,16 @@ class Ventana(QMainWindow):
     def __init__(self):
         QMainWindow.__init__(self)
         # Cargando la interfaz desde tu ruta
-        uic.loadUi(r"c:\Users\Admin\OneDrive\Escritorio\Integrales\Integrales-master\interfaz.ui", self)
+        uic.loadUi(r"c:\Users\Admin\OneDrive\Escritorio\Integrales_Proyect\Integrales-master\interfaz.ui", self)
         # Print
         self.btnCalcular.clicked.connect(self.hola)
         # Añadiendo iconos a botones
-        self.btnIntegralDef.setIcon(QIcon(r"c:\Users\Admin\OneDrive\Escritorio\Integrales\Integrales-master\assets\IntegralDefinida.png"))
-        self.btnIntegral.setIcon(QIcon(r"c:\Users\Admin\OneDrive\Escritorio\Integrales\Integrales-master\assets\Integral.png"))
-        self.btnDerivada.setIcon(QIcon(r"c:\Users\Admin\OneDrive\Escritorio\Integrales\Integrales-master\assets\Derivada.png"))
+        self.btnIntegralDef.setIcon(QIcon(r"c:\Users\Admin\OneDrive\Escritorio\Integrales_Proyect\Integrales-master\assets\IntegralDefinida.png"))
+        self.btnIntegral.setIcon(QIcon(r"c:\Users\Admin\OneDrive\Escritorio\Integrales_Proyect\Integrales-master\assets\Integral.png"))
+        self.btnDerivada.setIcon(QIcon(r"c:\Users\Admin\OneDrive\Escritorio\Integrales_Proyect\Integrales-master\assets\Derivada.png"))
         # Botones
-        self.btnC.clicked.connect(self.limpiar)
+        self.btnC.clicked.connect(self.eliminar_caracter)
+        self.btnCA.clicked.connect(self.limpiar_todo)
 
         # Variable para almacenar el contenido de la pantalla
         self.pantalla = ""
@@ -34,9 +35,31 @@ class Ventana(QMainWindow):
 
     def limpiar(self):
         self.pantalla = ""
-        self.textEdit.setText("")
+        self.textEdit.clear()  
 
     def hola(self):
         print("Hola mundo")
 
+    def eliminar_caracter(self):
 
+        # Obtener el contenido actual de la pantalla
+        contenido = self.textEdit.text()  
+
+        # Verificar si hay algo que eliminar
+        if contenido:
+            # Eliminar el último carácter de la cadena
+            contenido = contenido[:-1]
+            self.textEdit.setText(contenido)
+            
+            # Actualizar la variable pantalla después de eliminar el carácter
+            self.pantalla = contenido
+
+    def limpiar_todo(self):
+        self.textEdit.clear()  
+        self.pantalla = ""  
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    GUI = Ventana()
+    GUI.show()
+    sys.exit(app.exec_())
